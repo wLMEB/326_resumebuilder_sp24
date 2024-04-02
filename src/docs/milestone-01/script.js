1ba18b0e730a029fbcded11a1ab73cb57880fc56
Viral Rathod <viralrathod2015@vl965-172-31-14-231.wireless.umass.edu>
2024-04-01 22:29:04 -0400

    button to shwo bios

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -16,7 +16,7 @@ const nlBio = document.createElement("p");
 vrBio.innerHTML = "Viral Rathod: I am a sophomore that is familiar with frontend development and have worked with web applications as well as several mobile apps. I am interested in working with the user experience but able to contribute to the backend as well.";
 wlBio.innerHTML = "Wanqi Li: Hello, I'm a Junior at Umass Amherst. My interest lies in computer systems and databases. I'm looking forward to learning how to incorporate databases into web applications to serve as the backend. ";
 ljBio.innerHTML = "Lijiang Jiang: I am interested in art, and I have learned a little about human-computer interaction, and I may be able to contribute to the UI design of this project.";
-nlBio.innerHTML = "Nehal Linganur: TBD";
+nlBio.innerHTML = "Nehal Linganur: I am a senior and my majors are informatics and psychology. I have knowledge about HCI and HTML and CSS and I’m looking forward to gain more experience in working with the backend. I have some knowledge about user experiences and design which I think I can contribute to this project. ";
 
 const hideButton = document.createElement("button");
 hideButton.innerHTML = "Hide";
