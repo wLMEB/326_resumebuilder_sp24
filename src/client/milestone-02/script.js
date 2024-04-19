a21463fee0d6bf86411cc5317c0a40d4306f836d
wLMEB <liwanqi0248@gmail.com>
2024-04-18 22:34:06 -0400

    add a few more input boxes

--- src/client/milestone-02/script.js
+++ src/client/milestone-02/script.js
@@ -1,1 +1,15 @@
 const content = document.getElementById('content');
+
+let fields = ['First Name', 'Last Name', 'Email', 'Education'];
+
+function render(fieldName){
+    const fieldLabel = document.createElement('label');
+    const field = document.createElement('input');
+    fieldLabel.innerText=`${fieldName}: `;
+    
+    content.appendChild(fieldLabel);
+    content.appendChild(field);
+    content.appendChild(document.createElement('br'));
+}
+
+fields.forEach(field => render(field));
