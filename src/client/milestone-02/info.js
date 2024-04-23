63718213e1ee7e2f3f58e1310b861bc8a3f47a78
wLMEB <liwanqi0248@gmail.com>
2024-04-23 11:50:22 -0400

    Added comments

--- src/client/milestone-02/info.js
+++ src/client/milestone-02/info.js
@@ -1,9 +1,10 @@
 import { navigate } from "./main.js";
 import { render as tempRender } from "./temp.js";
 const content = document.getElementById('infoView');
+// Array of fields to be rendered
 let fields = ['First Name', 'Last Name', 'Email', 'Education'];
 
-function fieldGen(fieldName){
+function fieldGen(fieldName){ // Generate each field dynamically
     const fieldLabel = document.createElement('label');
     const field = document.createElement('input');
     fieldLabel.innerText=`${fieldName}: `;
@@ -12,8 +13,8 @@ function fieldGen(fieldName){
     content.appendChild(field);
     content.appendChild(document.createElement('br'));
 }
-function addButton(name, page, rend){
-    const button = document.createElement('button');
+function addButton(name, page, rend){ // Generate button dynamically and set their event listeners
+    const button = document.createElement('button'); // to navigate to the requested page
     button.innerText = `${name}`;
     content.appendChild(button);
     button.addEventListener("click", () => {
@@ -26,7 +27,7 @@ function addHeadernText(){
     header.innerText = "Input your information:";
     content.appendChild(header); 
 }
-function render(){
+function render(){              //render function called by other pages to render this page
     content.innerHTML = "";
     addHeadernText();
     fields.forEach(field => fieldGen(field));
