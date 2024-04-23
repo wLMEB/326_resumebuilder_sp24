2f383a470bf19d733ddc14dbc4947ddcf1dd03f0
Lijiang24 <lijiangjiang@umass.edu>
2024-04-23 16:54:44 -0400

    added some jsdoc

--- src/client/milestone-02/db.js
+++ src/client/milestone-02/db.js
@@ -3,7 +3,15 @@
 
 const db = new PouchDB("counters");
 
-
+/**
+ *  This asynchronously function adds a new information set to the database with the provided fieldname as the _id and the provided value as value.
+ * 
+ * @async
+ * @param {string} fieldname - The fieldname that will be used as the _id of the new document.
+ * @param {string} value - The value that will be stored in the 'value' field of the new document.
+ * @returns {Promise<void>} - A promise that resolves when the information has been successfully saved.
+ * @throws {Error} - Throws an error if the operation fails.
+ */
 export async function addInfo(fieldname, value){
     console.log(fieldname,value)
     await db.put({
