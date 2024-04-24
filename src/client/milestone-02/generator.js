5169b88fc7229a7055229f93ae8efe457ae3e344
Lijiang24 <lijiangjiang@umass.edu>
2024-04-24 12:17:01 -0400

    Update generator.js

--- src/client/milestone-02/generator.js
+++ src/client/milestone-02/generator.js
@@ -1,5 +1,11 @@
 let win = null;
 
+/**
+ * This function opens a new window and builds a resume in it. It creates an h1 element, sets its text, and appends it to the body of the new window's document.
+ * 
+ * @param {Object} fields - The fields to be included in the resume.
+ * @returns {void} This function does not return anything.
+ */
 function buildResume(fields){
     win = window.open("", "_blank", "width=800 ,height=1000 ,top=100,left=100" )
     const resume = win.document;
@@ -11,7 +17,11 @@ function buildResume(fields){
     console.log(fields);
 }
 
-
+/**
+ * This function returns the global 'win' variable, which is expected to be a Window object.
+ * 
+ * @returns {Window} The global 'win' variable.
+ */
 function getWindow(){
     return win;
 }
