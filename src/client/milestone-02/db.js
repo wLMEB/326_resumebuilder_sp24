c250ff799c635949c750890b6744bfbc8b6923e5
Lijiang24 <lijiangjiang@umass.edu>
2024-04-24 00:27:20 -0400

    Finish JSdoc for db.js

--- src/client/milestone-02/db.js
+++ src/client/milestone-02/db.js
@@ -4,12 +4,12 @@
 const db = new PouchDB("counters");
 
 /**
- *  This asynchronously function adds a new information set to the database with the provided fieldname as the _id and the provided value as value.
+ *  This asynchronously function adds a new information block to the database with the provided fieldname as the _id and the provided value as value.
  * 
  * @async
- * @param {string} fieldname - The fieldname that will be used as the _id of the information set.
- * @param {string} value - The value that will be stored in the 'value' field of the new information set.
- * @returns {Promise<void>} - A promise that resolves when the information has been successfully saved.
+ * @param {string} fieldname - The fieldname that will be used as the _id of the information block.
+ * @param {string} value - The value that will be stored in the 'value' field of the new information block.
+ * @returns {Promise<void>} - A promise that resolves when the information block has been successfully saved.
  * @throws {Error} - Throws an error if the operation fails.
  */
 export async function addInfo(fieldname, value){
@@ -21,10 +21,10 @@ export async function addInfo(fieldname, value){
 }
 
 /**
- * This function retrieves a information set from the database using the provided fieldname as the _id.
+ * This function retrieves a information block from the database using the provided fieldname as the _id.
  * 
- * @param {string} fieldname - The fieldname that will be used as the _id of the information set to retrieve information block.
- * @returns {Promise<any>}  - A promise that resolves when the information has been successfully got.
+ * @param {string} fieldname - The fieldname that will be used as the _id of the information block to retrieve information block.
+ * @returns {Promise<any>}  - A promise that resolves when the information block has been successfully got.
  * @throws {Error} - Throws an error if the operation fails.
  */
 export async function getInfo(fieldname){
@@ -32,6 +32,13 @@ export async function getInfo(fieldname){
     return doc.value;
 }
 
+/**
+ * This function deletes a information block from the database using the provided fieldname as the _id.
+ * 
+ * @param {string} fieldname - The fieldname that will be used as the _id of the information block to delete information block.
+ * @returns {Promise<void>}  - A promise that resolves when the information block has been successfully deleted.
+ * @throws {Error} - Throws an error if the operation fails.
+ */
 export async function deleteInfo(fieldname){
     const doc = await db.get(fieldname);
     await db.remove(doc);
