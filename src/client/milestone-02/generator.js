3743bb719c1c857c5e6c1471d24b67f24271f4f9
wLMEB <liwanqi0248@gmail.com>
2024-04-23 21:57:57 -0400

    popup resume page can be accessed and dowload

--- src/client/milestone-02/generator.js
+++ src/client/milestone-02/generator.js
@@ -1,7 +1,18 @@
+let win = null;
 
-
-function buildResume(){
- const resume = window.open('', ' ')
+function buildResume(fields){
+    win = window.open('', ' ')
+    const resume = win.document;
+    let header = resume.createElement('h1')
+    header.innerText = "testttt"
+    console.log(header);
+    resume.body.appendChild(header)
+    console.log(resume);
+    console.log(fields);
 }
 
-export {buildResume};
+
+function getWindow(){
+    return win;
+}
+export {buildResume,getWindow};
